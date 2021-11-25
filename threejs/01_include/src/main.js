f4acede4a98ef07188ce17a1d417454e63d50183
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2021-11-24 21:05:26 -0800

    fin include

--- threejs/01_include/src/main.js
+++ threejs/01_include/src/main.js
@@ -1,8 +1,18 @@
 /* Using npm-installed */
-import { Camera, Material, Texture } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { Camera, Material, Texture } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /* Using local files
 import { Camera, Material, Texture } from "./build/three.module.js";
 import { OribitControls } from "./examples/jsm/controls/OribitControls.js";
 */
+
+/* Using a CDN
+import { 
+  Camera, 
+  Material, 
+  Texture 
+} from "https://cdn.skypack.dev/three@0.132.2";
+import { 
+  OrbitContols 
+} from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js";
---
+++ threejs/init.sh
@@ -0,0 +1,5 @@
+# set up project
+npm init
+
+# install three
+npm install --save three
