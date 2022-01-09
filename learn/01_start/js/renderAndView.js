9d64aa659da7d8800adacb48aa5fc20e0eee41fd
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-01-08 16:28:53 -0800

    add spotlight

--- learn/01_start/js/renderAndView.js
+++ learn/01_start/js/renderAndView.js
@@ -14,7 +14,7 @@ function init() {
 
     // Ground plane
     const planeGeom = new THREE.PlaneGeometry(60, 20);
-    const planeMat = new THREE.MeshBasicMaterial({color: 0xAAAAAA});
+    const planeMat = new THREE.MeshLambertMaterial({color: 0xAAAAAA});
     let plane = new THREE.Mesh(planeGeom, planeMat);
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.set(15, 0, 0);
@@ -22,16 +22,14 @@ function init() {
 
     // Cube
     const cubeGeom = new THREE.BoxGeometry(4, 4, 4);
-    const cubeMat = new THREE.MeshBasicMaterial({color: 0xFF0000,
-                                                  wireframe: true});
+    const cubeMat = new THREE.MeshLambertMaterial({color: 0xFF0000});
     let cube = new THREE.Mesh(cubeGeom, cubeMat);
     cube.position.set(-4, 3, 0);
     scene.add(cube);
 
     // Sphere
     const sphereGeom = new THREE.SphereGeometry(4, 20, 20);
-    const sphereMat = new THREE.MeshBasicMaterial({color: 0x7777FF,
-                                                   wireframe: true});
+    const sphereMat = new THREE.MeshLambertMaterial({color: 0x7777FF});
     let sphere = new THREE.Mesh(sphereGeom, sphereMat);
     sphere.position.set(20, 4, 2);
     scene.add(sphere);
@@ -39,6 +37,15 @@ function init() {
     // Set camera
     camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
+
+    // Spotlight
+    let spotlight = new THREE.SpotLight(0xFFEECC);
+    spotlight.position.set(-40, 40, -15);
+    spotlight.castShadow = true;
+    spotlight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    spotlight.shadow.camera.far = 130;
+    spotlight.shadow.camera.near = 40;
+    scene.add(spotlight)
 
     document.getElementById('webgl-output').appendChild(renderer.domElement);
     renderer.render(scene, camera);
